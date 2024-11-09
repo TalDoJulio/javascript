2f5070401488fd63ca167ab7b5b5afa863724e9d
TalDoJulio <157721108+TalDoJulio@users.noreply.github.com>
2024-11-09 16:12:30 -0300

    condicoes e correcoes

---
+++ modulo-d/ex001.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu veiculo é ${vel}Km/h`)
+if (vel > 60) { 
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+} // Condição simples!
+console.log('Dirija sempre com cuidado!')
